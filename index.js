a3f3617f57acab4f7b203f884d2b891d7989f109
Alysia Pereira <alysiapereira5@gmail.com>
2020-01-20 20:00:52 -0500

    pdf populating

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ function promptUser() {
 
 function generateHTML(data) {
     return `<!DOCTYPE html>
-  <html lang="en">
+    <html lang="en">
      <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
